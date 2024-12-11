fa1e49c14f825f7afb51b53b18ec5fce0ee1f87a
Chris Korda <victimofleisure@users.noreply.github.com>
2024-12-11 01:14:28 +0100

    Fix missing resources for standard components
    AFX resource strings fail to load in a console app that uses MFC in static library, in Release build ONLY. The cause is that for console apps, the Visual Studio app wizard neglects to add compile-time directives for the standard components. The fix is:
    1. In Resource View, select Resource Includes.
    2. Under Compile-time directives, add the code to include the standard components.

--- trunk/MidiToLily/stdafx.h
+++ trunk/MidiToLily/stdafx.h
@@ -29,11 +29,6 @@
 #include <iostream>
 
 
-#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
-
-#include <atlbase.h>
-#include <atlstr.h>
-
 // TODO: reference additional headers your program requires here
 #include "Wrapx64.h"
 #include "Round.h"
